66c97092124381f5f3a3a7018445f83f632f0a31
Alesha <VAlesha@hq.telesens.lan>
2020-03-03 13:51:01 +0200

    admin

--- src/app/routing/index.js
+++ src/app/routing/index.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import App from '../App';
 import Admin from './privateRouting/admin';
+import AdminPage from '../pages/Admin';
 
 function Router(props) {
   return (
         <Fragment>
             <Switch>
-            <Route component = {App} fallBack = {'/56756'} path = '/'/>
+            {/* <Route component = {App} fallBack = {'/56756'} path = '/'/> */}
+            <Route component = {AdminPage} path = '/'/>
             </Switch>
         </Fragment>
   );
