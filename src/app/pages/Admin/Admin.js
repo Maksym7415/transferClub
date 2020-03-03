d9157a7ea778019f1fb28a5e9c0fe2949a3e59d5
Alesha <VAlesha@hq.telesens.lan>
2020-03-03 14:40:58 +0200

    profile

--- src/app/pages/Admin/Admin.js
+++ src/app/pages/Admin/Admin.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
@@ -16,106 +17,21 @@ import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
+
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import HowToReg from '@material-ui/icons/HowToReg';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    // maxWidth: '1200px',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
-  wrapper: {
-    maxWidth: '1200px',
-
-  },
-}));
 
 function AdminPage(props) {
   const classes = useStyles();
@@ -167,27 +83,24 @@ function AdminPage(props) {
           </IconButton>
         </div>
         <Divider/>
+        <Link to = '/myProfile'>
+          <ListItem button>
+            <ListItemIcon>
+              <AccountCircle />
+            </ListItemIcon>
+            <ListItemText primary="My Profile" />
+          </ListItem>
+        </Link>
         <ListItem button>
           <ListItemIcon>
-            <ShoppingCartIcon />
+            <HowToReg />
           </ListItemIcon>
-          <ListItemText primary="Orders" />
+          <ListItemText primary="Registrations" />
         </ListItem>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
             <Grid item xs={12}>
               <Paper className={classes.paper}>
               <Table size="small">
@@ -202,7 +115,6 @@ function AdminPage(props) {
               </Table>
               </Paper>
             </Grid>
-          </Grid>
         </Container>
       </main>
     </div>
--- src/app/routing/index.js
+++ src/app/routing/index.js
@@ -1,17 +1,33 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
+
 
 import App from '../App';
 import Admin from './privateRouting/admin';
-import AdminPage from '../pages/Admin';
+import AdminPage from '../pages/Admin/Admin';
+import ProgilePage from '../pages/Admin/Profile';
+
+const theme = makeStyles((theme) => ({
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+}));
 
 function Router(props) {
   return (
         <Fragment>
-            <Switch>
-            {/* <Route component = {App} fallBack = {'/56756'} path = '/'/> */}
-            <Route component = {AdminPage} path = '/'/>
-            </Switch>
+            <ThemeProvider theme={theme}> */}
+                <Switch>
+                {/* <Route component = {App} fallBack = {'/56756'} path = '/'/> */}
+                <Route component = {AdminPage} path = '/'/>
+                <Route component = {ProgilePage} path = '/myProfile'/>
+                </Switch>
+            </ThemeProvider>
         </Fragment>
   );
 }
