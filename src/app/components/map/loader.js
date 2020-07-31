90659972bd3f6ac8986abaa4755a13de8cf931ee
Maksym7415 <49568332+Maksym7415@users.noreply.github.com>
2020-07-31 21:36:03 +0300

    Update loader.js

--- src/app/components/map/loader.js
+++ src/app/components/map/loader.js
@@ -4,7 +4,7 @@ import { LoadScript } from '@react-google-maps/api';
 const MapLoader = (props) => (
   <LoadScript
       id="script-loader"
-      // googleMapsApiKey="AIzaSyDxy2xkxk7zDKEGtsXtIGEiaMdBVV3vH3Q"
+      googleMapsApiKey=""
       libraries={['places']}
       url='https://maps.googleapis.com/maps/api/directions/json'
     >
@@ -12,6 +12,3 @@ const MapLoader = (props) => (
   </LoadScript>
 );
 export default MapLoader;
-
-
-// my AIzaSyD530nQRjKmRP41ba_vm7YC_G90OBGqD8M
